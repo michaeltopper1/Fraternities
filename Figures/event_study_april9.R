097b2ce8a2de6286ae3c0e2a8e76860e5990b683
miketopper123 <miketopper123@gmail.com>
2021-04-08 22:41:56 -0700

    created event study graphs

--- Figures/event_study_april9.R
+++ Figures/event_study_april9.R
@@ -3,11 +3,11 @@ library(fixest)
 library(modelsummary)
 library(ifc)
 library(lubridate)
+library(broom)
 
 weekly_crime <- read_csv("Created Data/xMaster_data_2021/weekly_panel.csv")
-weekly_crime_weekends <- read_csv("Created Data/xMaster_data_2021/weekly_panel_weekends.csv")
 
-es <- ifc::event_study_week(weekly_crime, 8, reference_week = 2)
+es <- ifc::event_study_week(weekly_crime, 8, reference_week = 1)
 
 clean_es <- function(data) {
   cleaned_es <- data %>% 
@@ -17,46 +17,67 @@ clean_es <- function(data) {
   return(cleaned_es)
 }
 
+event_study_func <- function(x, window_size) {
+  coefs <- tidy(x, conf.int =T)[1:(2*window_size), ]
+  coefs <- coefs %>% 
+    add_row(.before = window_size)
+  event_window <- c(-window_size:window_size)
+  event_window <- as_tibble(event_window)  
+  coefs <- tibble(coefs, event_window)
+  coefs <- coefs %>% 
+    mutate(across(.cols = -c(term), ~ ifelse(is.na(.), 0, .))) 
+  coefs <- coefs[2:(2*window_size),]
+  plot <- coefs %>% 
+    ggplot(aes(x = value, y = estimate), alpha = 0.8) +
+    geom_path(linetype = "dashed", alpha = 0.8) +
+    geom_point() +
+    geom_errorbar(aes(x = value, ymin = conf.low, ymax = conf.high), alpha = 0.8) +
+    geom_rect(aes(xmin = -0.5, xmax =0.5, ymin = -Inf, ymax = Inf), fill = "grey", alpha = 0.03) +
+    scale_x_continuous(labels = c(-window_size:window_size), breaks = c(-window_size:window_size)) +
+    labs(x = "", y = "") +
+    ggthemes::theme_calc()
+  return(plot)
+}
+
+event_study_func(alcohol_es, 8) +
+  expand_limits(y = c(-.95, .75)) 
+
 es <- clean_es(es)
 
-x <- es %>% 
-  # filter(!(str_detect(university, "_2$"))) %>% 
-  # filter(university != "Miami University-Oxford") %>% 
-  feols(c(alcohol_offense_per100) ~ treatment_minus_5 +
-          treatment_minus_4 + treatment_minus_3 + treatment_minus_2 + treatment_minus_1 + treatment + 
-          treatment_plus_1 + treatment_plus_2 + treatment_plus_3 + treatment_plus_4 + treatment_plus_5 +  ftime_total_undergrad + total_undergrad_asian +
-          total_undergrad_hispanic + total_undergrad_black| uni_month + year, cluster = ~university, data = .) %>% summary()
-x %>% 
-  coefplot()
+ihs_alcohol_es <- es %>% 
+  feols(ihs_alcohol_offense ~ treatment_minus_8 + treatment_minus_7 + treatment_minus_6 + treatment_minus_5 + 
+                    treatment_minus_4 + treatment_minus_3 + treatment_minus_2 + treatment_minus_1 + treatment + 
+                    treatment_plus_1 + treatment_plus_2 + treatment_plus_3 + treatment_plus_4 + treatment_plus_5 + treatment_plus_6 +
+                    treatment_plus_7 + treatment_plus_8 +  ftime_total_undergrad + total_undergrad_asian +
+                    total_undergrad_hispanic + total_undergrad_black + graduation_rate_total_cohort_| uni_month +  year , cluster = ~university, data = .) 
 
-alcohol_es <- es %>% 
-  group_by(university, year) %>% 
-  mutate(uni_year = cur_group_id()) %>% 
-  ungroup() %>% 
+per100_alcohol_es <- es %>% 
   feols(alcohol_offense_per100 ~ treatment_minus_8 + treatment_minus_7 + treatment_minus_6 + treatment_minus_5 + 
           treatment_minus_4 + treatment_minus_3 + treatment_minus_2 + treatment_minus_1 + treatment + 
           treatment_plus_1 + treatment_plus_2 + treatment_plus_3 + treatment_plus_4 + treatment_plus_5 + treatment_plus_6 +
-          treatment_plus_7 + treatment_plus_8 | uni_month +  uni_year , cluster = ~university, data = .) 
-alcohol_es %>% 
-  coefplot()
+          treatment_plus_7 + treatment_plus_8 +  ftime_total_undergrad + total_undergrad_asian +
+          total_undergrad_hispanic + total_undergrad_black + graduation_rate_total_cohort_| uni_month +  year , cluster = ~university, data = .) 
 
-other_es <- es %>% 
-  feols(theft_per100 ~ treatment_minus_8 + treatment_minus_7 + treatment_minus_6 + treatment_minus_5 + 
+
+ihs_sexual_assault_es <- es %>% 
+  feols(ihs_sexual_assault ~ treatment_minus_8 + treatment_minus_7 + treatment_minus_6 + treatment_minus_5 + 
           treatment_minus_4 + treatment_minus_3 + treatment_minus_2 + treatment_minus_1 + treatment + 
           treatment_plus_1 + treatment_plus_2 + treatment_plus_3 + treatment_plus_4 + treatment_plus_5 + treatment_plus_6 +
-          treatment_plus_7 + treatment_plus_8| uni_month + year, cluster = ~university, data = .) 
-other_es %>% 
-  coefplot
-es %>% 
-  group_by(university) %>% 
-  count(treatment_minus_2) %>% View()
+          treatment_plus_7 + treatment_plus_8 +  ftime_total_undergrad + total_undergrad_asian +
+          total_undergrad_hispanic + total_undergrad_black + graduation_rate_total_cohort_| uni_month +  year , cluster = ~university, data = .) 
+per100_sexual_assault_es <- es %>% 
+  feols(sexual_assault_per100 ~ treatment_minus_8 + treatment_minus_7 + treatment_minus_6 + treatment_minus_5 + 
+          treatment_minus_4 + treatment_minus_3 + treatment_minus_2 + treatment_minus_1 + treatment + 
+          treatment_plus_1 + treatment_plus_2 + treatment_plus_3 + treatment_plus_4 + treatment_plus_5 + treatment_plus_6 +
+          treatment_plus_7 + treatment_plus_8 +  ftime_total_undergrad + total_undergrad_asian +
+          total_undergrad_hispanic + total_undergrad_black + graduation_rate_total_cohort_| uni_month +  year , cluster = ~university, data = .) 
 
-es %>% 
-  colnames()
+alcohol_es_100 <- event_study_func(per100_alcohol_es, 8) + 
+  expand_limits(y = c(-20, 20))
+alcohol_es_ihs <- event_study_func(ihs_alcohol_es, 8) +
+  expand_limits(y = c(-1, 1))
 
-es %>% 
-  group_by(university) %>% 
-  datasummary_skim()
-
-es %>% 
-  filter(alcohol_offense >20) %>% View()
+sexual_assault_es_100 <- event_study_func(per100_sexual_assault_es, 8) + 
+  expand_limits(y = c(-7, 5))
+sexual_assault_es_ihs <- event_study_func(ihs_sexual_assault_es, 8)  + 
+  expand_limits(y = c(-1,1))
