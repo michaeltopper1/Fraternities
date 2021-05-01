0d51db6718ac936bbab6a70bfb4c0e78f4bc4d3d
miketopper123 <miketopper123@gmail.com>
2021-05-01 09:49:18 -0700

    changed file paths to relative paths

--- Code/Cleaning_crime_log_files/xxRunForNewData.R
+++ Code/Cleaning_crime_log_files/xxRunForNewData.R
@@ -9,13 +9,12 @@ library(tidyverse)
 
 
 ##IPEDS first
-source("/Users/michaeltopper/Desktop/Fraternities and Sexual Assault/Code/Cleaning_IPEDS/clean_ipeds_race_1.R")
-source("/Users/michaeltopper/Desktop/Fraternities and Sexual Assault/Code/Cleaning_IPEDS/clean_ipeds_ic_freq_2.R")
-source("/Users/michaeltopper/Desktop/Fraternities and Sexual Assault/Code/Cleaning_IPEDS/clean_ipeds_grad_financial_3.R")
-source("/Users/michaeltopper/Desktop/Fraternities and Sexual Assault/Code/Cleaning_IPEDS/clean_ipeds_sat_4.R")
-source("/Users/michaeltopper/Desktop/Fraternities and Sexual Assault/Code/Cleaning_IPEDS/merge_ipeds.R")
+source("Code/Cleaning_IPEDS/clean_ipeds_race_1.R")
+source("Code/Cleaning_IPEDS/clean_ipeds_ic_freq_2.R")
+source("Code/Cleaning_IPEDS/clean_ipeds_grad_financial_3.R")
+source("Code/Cleaning_IPEDS/clean_ipeds_sat_4.R")
+source("Code/Cleaning_IPEDS/merge_ipeds.R")
 
 
 ## cleaning panel for daily and monthly
-source("/Users/michaeltopper/Desktop/Fraternities and Sexual Assault/Code/Cleaning_crime_log_files/create_panel_2.R")
-source("/Users/michaeltopper/Desktop/Fraternities and Sexual Assault/Code/Cleaning_crime_log_files/clean_for_analysis_3.R")
+source("Code/Cleaning_crime_log_files/create_panel_2.R")
