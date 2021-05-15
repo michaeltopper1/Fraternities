fc569b4759fe9d6cd10e19496c01076f61a8eae3
miketopper123 <miketopper123@gmail.com>
2021-05-15 14:17:00 -0700

    creates data where i only have academic calendar days

---
+++ Code/Cleaning_crime_log_files/create_panel_semesters_2.R
@@ -0,0 +1,273 @@
+## Purpose of script: replaces the create_panel_2 document 
+##
+## Author: Michael Topper
+##
+## Date Last Edited: 2021-05-15
+##
+
+library(tidyverse)
+library(readxl)
+
+
+
+# Sourcing the previous script to get counts ------------------------------
+
+## sourcing the clean_daily_crime_logs file which connects all of the cleaned crime logs togther
+## and then creates new variables, and then collapses them
+source("Code/Cleaning_crime_log_files/append_daily_crime_logs_1.R")
+
+
+
+## this creates a short vector of the distinct universities in my cleaned crime data
+universities <- appended_crime_logs %>% distinct(university) %>% pull(university)
+
+
+
+
+
+# Creating Academic Calendar panels by semester ---------------------------
+
+## pulling in the academic calendars
+academic_calendars <- read_csv("Data/academic_calendars.csv") %>% janitor::clean_names()
+
+## moving all calendar dates to start in 2014
+academic_calendars <- academic_calendars %>% 
+  extract(fall_start, "fall_start", "(\\d{1,2}/\\d{1,2})") %>% 
+  mutate(fall_start = paste0(fall_start, "/", "14")) %>% 
+  extract(fall_end, "fall_end", "(\\d{1,2}/\\d{1,2})") %>% 
+  mutate(fall_end = paste0(fall_end, "/", "14")) %>% 
+  extract(spring_start, "spring_start", "(\\d{1,2}/\\d{1,2})") %>% 
+  mutate(spring_start = paste0(spring_start, "/", "14")) %>% 
+  extract(spring_end, "spring_end", "(\\d{1,2}/\\d{1,2})") %>% 
+  mutate(spring_end = paste0(spring_end, "/", "14")) %>% 
+  mutate(across(c(-university), ~mdy(.))) 
+
+## adding 1 week to the fall_start of the academic calendar to account for welcome week.
+## adding 1 week to the end of spring for conservative ness
+academic_calendars <- academic_calendars %>% 
+  mutate(fall_start = fall_start - days(7), spring_end = spring_end + days(7)) %>% 
+  mutate(spring_start = case_when(
+    spring_start <= as.Date("1/7/14") ~spring_start,
+    spring_start > as.Date("1/7/14") ~spring_start - days(7)
+  ))
+
+
+## looping through all semesters to expand the grid
+spring_semester <- -1
+for (i in 0:5) {
+  spring_semester <- spring_semester + 2
+  name <- paste0("semester_", spring_semester)
+  calendar <- academic_calendars %>% 
+    mutate(spring_start = spring_start + years(i), spring_end = spring_end + years(i)) %>% 
+    group_by(university) %>% 
+    do(data.frame(semester_number = spring_semester, date = seq(.$spring_start, .$spring_end , by= "day")))
+  assign(name, calendar)
+}
+
+
+## looping through all semesters to expand the grid.
+winter_semester <- 0
+for (i in 0:5) {
+  winter_semester <- winter_semester + 2
+  name <- paste0("semester_", winter_semester)
+  calendar <- academic_calendars %>% 
+    mutate(fall_start = fall_start + years(i), fall_end = fall_end + years(i)) %>% 
+    group_by(university) %>% 
+    do(data.frame(semester_number = winter_semester, date = seq(.$fall_start, .$fall_end , by= "day")))
+  assign(name, calendar)
+}
+
+## appending all semesters
+panel <- bind_rows(semester_1, semester_2,
+                   semester_3, semester_4,
+                   semester_5, semester_6,
+                   semester_7, semester_8,
+                   semester_9, semester_10,
+                   semester_11, semester_12)
+
+## removing to save data
+rm(semester_1, semester_2,
+   semester_3, semester_4,
+   semester_5, semester_6,
+   semester_7, semester_8,
+   semester_9, semester_10,
+   semester_11, semester_12)
+
+## filtering for only the universities that I have data for at this time.
+panel <- panel %>% 
+  filter(university %in% universities)
+
+
+
+
+
+
+# Merging the crime count data with the panel -----------------------------
+
+## Next Step: Now I need to merge the two datas together - the collapsed data and the large panel.
+## joining together the dates and the counts to make a long panel
+daily_panel <- panel %>% 
+  left_join(collapsed_data_daily, by = c("university" = "university", "date" = "date_reported")) %>% 
+  mutate(year = year(date), month = month(date), day = day(date))
+
+
+
+
+
+# Changing NAs to 0s where applicable + missing data ----------------------
+
+## now I need to pull in data that keeps track of which years I have missing
+missing_years <- read_xlsx("Data/campus_daily_crime_log/crime_log_list.xlsx",
+                           sheet = "missing_years") %>% janitor::clean_names()
+
+
+## creates a list of schools missing year 2014
+not_missing_2014 <- missing_years %>% 
+  filter(missing_2014 == 0) %>% 
+  pull(university)
+
+
+## replaces all days with NAs with 0s if i had data for that year
+## i did this because i can assume there were 0 reports on a date that there was no crime in years i have
+daily_panel <- daily_panel %>% 
+  mutate(across(c("alcohol_offense", "sexual_assault", "theft", "drug_offense","robbery_burglary", "alcohol_offense_strict", "noise_offense", "rape"), 
+                ~ifelse(year== 2014 & is.na(.) & university %in% not_missing_2014,
+                        0, .))) %>% 
+  mutate(across(c("alcohol_offense", "sexual_assault", "theft", "drug_offense","robbery_burglary", "alcohol_offense_strict", "noise_offense", "rape"), 
+                ~ifelse(year >= 2015 & is.na(.), 0, .)))## changes all missings from 2015 onwards to 0s- careful of North Florida!
+
+## North Florida I only have information from July 2015- onwards. Here I am accounting for this
+daily_panel <- daily_panel %>% 
+  mutate(weekday = wday(date, label = T)) %>%  ## gets the weekday label
+  mutate(across(c("alcohol_offense", "sexual_assault", "theft", "drug_offense","robbery_burglary", "alcohol_offense_strict", "noise_offense", "rape"), ~ifelse(
+    year == 2015 & month < 7 & university == "University of North Florida",
+    NA, .
+  ))) 
+
+
+
+
+# Pulling in school closure data ------------------------------------------
+
+##### Last Step: pulling in the closure data and merging it with this final_panel
+closures <- read_xlsx("Data/closure_spreadsheet_final_2019.xlsx") %>% 
+  janitor::clean_names() %>% 
+  select(university, date, deadline, date2, deadline2,
+         university_enacted_1, university_enacted_2, reason1, reason2) %>% 
+  rename("closure_1" = date,
+         "closure_1_end" = deadline,
+         "closure_2" = date2,
+         "closure_2_end" = deadline2)
+
+
+
+daily_panel <- daily_panel %>% 
+  left_join(closures, by = c("university" = "university")) %>% 
+  mutate(treatment = case_when(
+    (!is.na(closure_1) & !is.na(closure_1_end)) & (date >= closure_1 & date < closure_1_end) ~ 1,
+    (!is.na(closure_2) & !is.na(closure_2_end)) & (date >= closure_2 & date < closure_2_end) ~ 1,
+    TRUE ~as.double(0)
+  )) %>% 
+  mutate(university_enacted = case_when(
+    university_enacted_1 == 1 & treatment == 1 ~ 1,
+    university_enacted_2 == 1 & treatment == 1 ~ 1,
+    TRUE ~as.double(0)
+  ))
+
+
+## Weekly panel
+weekly_panel <- daily_panel %>% 
+  group_by(university, week = cut(date, "week")) %>% 
+  summarize(across(c(sexual_assault,
+                     alcohol_offense,
+                     drug_offense,
+                     theft,
+                     robbery_burglary,
+                     alcohol_offense_strict,
+                     noise_offense,
+                     treatment,
+                     rape), ~ sum(., na.rm  = T))) %>% 
+  mutate(week = ymd(week)) %>% 
+  mutate(week_id = wday(week, label = T)) %>% 
+  ungroup() 
+
+## rejoining the closure data and since i took the sum of treatment, creating two variables: 1 that is a percentage of week treated and one that is an indicator
+weekly_panel <- weekly_panel %>% 
+  left_join(closures, by = c("university" = "university")) %>% 
+  mutate(treatment_percent = treatment/7) %>% 
+  mutate(treatment = ifelse(treatment > 0, 1, 0)) %>% 
+  mutate(year = year(week))
+
+## yearly panel:
+yearly_panel <- daily_panel %>% 
+  group_by(year, university) %>% 
+  summarize(sexual_assault = sum(sexual_assault),
+            alcohol_offense = sum(alcohol_offense),
+            drug_offense = sum(drug_offense),
+            theft = sum(theft),
+            robbery_burglary = sum(robbery_burglary),
+            alcohol_offense_strict = sum(alcohol_offense_strict),
+            noise_offense = sum(noise_offense),
+            treatment = mean(treatment),
+            rape = sum(rape)) %>%
+  ungroup() %>% arrange(university, year) 
+
+
+
+
+# Adding in IPEDS ---------------------------------------------------------
+
+### adding in IPEDS data
+ipeds <- read_csv("Created Data/IPEDS/ipeds_final.csv") %>% 
+  filter(year > 2013)
+
+daily_panel <-  daily_panel %>% 
+  left_join(ipeds, by = c("university" = "university", 'year' = 'year')) %>% 
+  mutate(across(c(sexual_assault, alcohol_offense,
+                  theft, robbery_burglary, drug_offense, rape), list(ihs = ifc::ihs_transform),
+                .names = "{.fn}_{.col}")) %>% 
+  group_by(university, month) %>% 
+  mutate(uni_month = cur_group_id()) %>% 
+  ungroup() %>% 
+  mutate(across(c(sexual_assault, alcohol_offense,
+                  theft, robbery_burglary, drug_offense, rape), ~./total_enrollment * 25000,
+                .names = '{.col}_per25')) %>% 
+  group_by(university, semester_number) %>% 
+  mutate(uni_semester = cur_group_id()) %>% 
+  ungroup() %>% 
+  filter(year > 2013) 
+
+yearly_panel <- yearly_panel %>% 
+  left_join(ipeds, by= c("university" = "university", 'year' = "year"))
+
+weekly_panel <- weekly_panel %>% 
+    left_join(ipeds, by = c("university" = "university", 'year' = 'year')) %>% 
+    mutate(month = month(week), year = year(week)) %>% 
+    mutate(across(c(sexual_assault, alcohol_offense,
+                    theft, robbery_burglary, drug_offense, rape), list(ihs = ifc::ihs_transform),
+                  .names = "{.fn}_{.col}")) %>% 
+    group_by(university, month) %>% 
+    mutate(uni_month = cur_group_id()) %>% 
+    ungroup() %>% 
+    mutate(across(c(sexual_assault, alcohol_offense,
+                    theft, robbery_burglary, drug_offense, rape), ~./total_enrollment * 25000,
+                  .names = '{.col}_per25')) %>% 
+    group_by(university, year) %>% 
+    mutate(uni_year = cur_group_id()) %>% 
+    ungroup() %>% 
+    filter(year > 2013)
+
+
+
+# Writing to csv ----------------------------------------------------------
+
+## Note that all of these are only the academic calendars. These are going to be my final
+## daily panel
+write_csv(daily_panel, file = "Created Data/xMaster_data_2021/daily_panel.csv")
+
+## weekly panel
+write_csv(weekly_panel, file = "Created Data/xMaster_data_2021/weekly_panel.csv")
+
+## yearly panel
+write_csv(yearly_panel, file = "Created Data/xMaster_data_2021/yearly_panel.csv")
+
