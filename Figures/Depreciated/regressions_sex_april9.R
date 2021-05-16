8dc5464d7bfac9e91444b39564726d5d0fb20f9c
miketopper123 <miketopper123@gmail.com>
2021-05-15 23:44:00 -0700

    depreciated output

---
+++ Figures/Depreciated/regressions_sex_april9.R
@@ -0,0 +1,67 @@
+## Purpose of script: Gives the regressions of sexual assault. This is for weekends only. E.g. Daily and Weekly totals are only based on
+## Fri/Sat/Sun
+## Author: Michael Topper
+##
+## Date Last Edited: 2021-04-08
+##
+
+library(tidyverse)
+library(fixest) 
+library(modelsummary)
+library(ifc)
+library(lubridate)
+library(kableExtra)
+
+
+daily_crime <- read_csv("Created Data/xMaster_data_2021/daily_panel_nosummer.csv",
+                        guess_max = 50000)
+weekly_crime <- read_csv("Created Data/xMaster_data_2021/weekly_panel_nosummer.csv",
+                         guess_max = 50000)
+
+
+
+daily_crime <- daily_crime %>% 
+  mutate(across(c(ftime_total_undergrad, total_undergrad_asian,
+                  total_undergrad_black, total_undergrad_hispanic),
+                ~ ./total_students_undergrad)) 
+weekly_crime <- weekly_crime %>% 
+  mutate(across(c(ftime_total_undergrad, total_undergrad_asian,
+                  total_undergrad_black, total_undergrad_hispanic),
+                ~ ./total_students_undergrad)) 
+
+
+
+
+per100_d <- daily_crime %>% 
+  feols(sexual_assault_per100 ~ treatment + ftime_total_undergrad + 
+          total_undergrad_black + total_undergrad_asian + total_undergrad_hispanic + graduation_rate_total_cohort_|
+          uni_month + year + weekday, cluster = ~university, data = .) 
+ihs_d <- daily_crime %>% 
+  feols(ihs_sexual_assault ~ treatment + ftime_total_undergrad + 
+          total_undergrad_black + total_undergrad_asian + total_undergrad_hispanic + graduation_rate_total_cohort_|
+          uni_month + year + weekday, cluster = ~university, data = .) 
+
+
+per100_w <- weekly_crime %>% 
+  feols(sexual_assault_per100 ~ treatment + ftime_total_undergrad + 
+          total_undergrad_black + total_undergrad_asian + total_undergrad_hispanic + graduation_rate_total_cohort_|
+          uni_month + year, cluster = ~university, data = .) 
+ihs_w <- weekly_crime %>% 
+  feols(ihs_sexual_assault ~ treatment + ftime_total_undergrad + 
+          total_undergrad_black + total_undergrad_asian + total_undergrad_hispanic + graduation_rate_total_cohort_|
+          uni_month + year, cluster = ~university, data = .) 
+
+
+
+sex_models <- list("Per 100k Students" = per100_d, "IHS(Sexual Assault Report)" = ihs_d,
+                   "Per 100k Students"= per100_w,
+                   "IHS(Sexual Assault Report)" = ihs_w)
+sex_ols <- modelsummary(sex_models, stars = T, gof_omit = 'DF|Deviance|AIC|BIC|Log|R2 Within|R2 Ps',
+                        coef_map = c("treatment" = "Moratorium",
+                                     "ftime_total_undergrad" = "Fraction Full-time Undergrad",
+                                     "total_undergrad_black" = "Fraction Undergrad Black",
+                                     "total_undergrad_asian" = "Fraction Undergrad Asian",
+                                     "total_undergrad_hispanic" = "Fraction Undergrad Hispanic",
+                                     "graduation_rate_total_cohort_" = "Graduation Rate"),
+                        output = "kableExtra",title = "Effect of fraternity moratoria on reports of sexual assaults (excluding summer)") %>% 
+  add_header_above(c(" " = 1, "Daily Level" = 2, "Weekly Level" = 2))
