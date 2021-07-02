c894fe011e3f2f66807b5cab1c7b086e8a467073
miketopper123 <miketopper123@gmail.com>
2021-07-01 22:14:43 -0700

    updates to map with size

--- Figures/map.R
+++ Figures/map.R
@@ -10,18 +10,20 @@ library(maps)
 
 
 daily_crime <- read_csv("Created Data/xMaster_data_2021/daily_panel.csv")
+ifc_fraternities <- read_csv("Data/ifc_fraternity_count.csv")
 
 us_states <- map_data("state") %>% 
   as_tibble()
 
 universities <- daily_crime %>% 
   mutate(ever_treated = ifelse(university %in% ifc::untreated_universities(), "No Moratorium", "Experienced Moratorium")) %>% 
-  distinct(university, ever_treated, longitude, latitude)
-
+  distinct(university, ever_treated, longitude, latitude) %>% 
+  left_join(ifc_fraternities)
 map_of_schools <- us_states %>% 
   ggplot(aes(long, lat, group = group)) +
   geom_polygon(fill = "white") +
   borders("state") +
-  geom_point(data = universities, aes(longitude, latitude, group = university, color = as_factor(ever_treated)), size = 2.5) +
-  labs(color = "", size = "") +
-  ggthemes::theme_map()
+  geom_point(data = universities, aes(longitude, latitude, group = university, color = as_factor(ever_treated), size = most_recent)) +
+  labs(color = "", size = "IFC size") +
+  ggthemes::theme_map() +
+  scale_size_binned()
