04df4cb06775030009e3cd71d85c2f03021ab95d
miketopper123 <miketopper123@gmail.com>
2021-07-02 14:42:06 -0700

    hetero effects on alcohol

---
+++ Figures/heteroeffect_trigger_alc.R
@@ -0,0 +1,51 @@
+## Purpose of script:
+##
+## Author: Michael Topper
+##
+## Date Last Edited: 2021-06-23
+##
+## Purpose of script:
+##
+## Author: Michael Topper
+##
+## Date Last Edited: 2021-06-23
+##
+
+library(tidyverse)
+library(fixest)
+library(modelsummary)
+library(lubridate)
+
+daily_crime <- read_csv("Created Data/xMaster_data_2021/daily_panel.csv")
+weekends <- c("Fri", "Sat", "Sun")
+daily_crime <- daily_crime %>% 
+  mutate(across(c(reason1, reason2), ~ifelse(is.na(.), "untreated", .))) %>% 
+  mutate(reason_sexual_assault = ifelse((reason1 == 'sexual assault' & (date >= closure_1 & date < closure_1_end)  & treatment == 1) 
+                                        | (reason2 == 'sexual assault' & (date >= closure_2 & date < closure_2_end) & treatment == 1) , 1, 0)) %>% 
+  mutate(reason_death = ifelse((reason1 == 'death' & (date >= closure_1 & date < closure_1_end)  & treatment == 1) 
+                               | (reason2 == 'death' & (date >= closure_2 & date < closure_2_end) & treatment == 1) , 1, 0)) %>% 
+  mutate(reason_behavior = ifelse((reason1 == 'behavior' & (date >= closure_1 & date < closure_1_end)  & treatment == 1) 
+                                  | (reason2 == 'behavior' & (date >= closure_2 & date < closure_2_end) & treatment == 1) , 1, 0)) %>% 
+  mutate(reason_unknown = ifelse((reason1 == 'unknown' & (date >= closure_1 & date < closure_1_end)  & treatment == 1) 
+                                 | (reason2 == 'unknown' & (date >= closure_2 & date < closure_2_end) & treatment == 1) , 1, 0))
+
+hetero_reasons_death <- daily_crime %>% 
+  feols(c(alcohol_offense_per25) ~ sw(treatment:reason_sexual_assault,
+                                     treatment:reason_death,
+                                     treatment:reason_behavior,
+                                     treatment:reason_unknown) |
+          uni_semester + weekday, cluster = ~university, data = .) 
+names(hetero_reasons_death) <- c("Sexual Assault", "Death of Student", "Behavior Violation", "Unknown")
+
+mean_alcohol <- mean(daily_crime$alcohol_offense_per25, na.rm = T)
+mean_alcohol <- tribble(~sex, ~new, ~x, ~y, ~z,
+                       "Mean of Alcohol Offense Per 25k ",mean_alcohol, mean_alcohol, mean_alcohol, mean_alcohol)
+attr(mean_alcohol, 'position') <- c(9)
+hetero_reasons_death %>% 
+  modelsummary(stars = T, gof_omit ="^R|^AIC|^BIC|^Log",
+               coef_rename = c("treatment:reason_sexual_assault" = "Moratorium x Triggering Sexual Assault",
+                               "treatment:reason_death" = "Moratorium x Triggering Death of Student",
+                               "treatment:reason_behavior" = "Motatorium x Triggering Behavior Violation",
+                               "treatment:reason_unknown" = "Moratorium x Triggering Event Unknown"),
+               title = "Effect of fraternity moratoria on alcohol offenses by triggering event.", add_rows = mean_alcohol) %>% 
+  kableExtra::add_header_above(c(" " = 1, "Triggering Event" = 4))
