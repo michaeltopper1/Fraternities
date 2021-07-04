d21ca4b4082af1f70d96ae9d1ae5e153ac31b630
miketopper123 <miketopper123@gmail.com>
2021-07-03 22:57:35 -0700

    fixed the regex to make a better match. file paths changed too

--- Data/campus_daily_crime_log/Syracuse/read_clean_data.R
+++ Data/campus_daily_crime_log/Syracuse/read_clean_data.R
@@ -12,7 +12,7 @@ library(lubridate)
 library(readxl)
 
 ## 2014
-path_2014 <- "/Users/michaeltopper/Desktop/Fraternities and Sexual Assault/Data/campus_daily_crime_log/Syracuse/2014 DCL.xlsx"
+path_2014 <- "Data/campus_daily_crime_log/Syracuse/2014 DCL.xlsx"
 syracuse_14 <- read_xlsx(path_2014) %>% 
   janitor::clean_names()
 syracuse_14 <- syracuse_14 %>% 
@@ -48,9 +48,9 @@ syracuse_15 <- syracuse_15 %>%
 
 
 ## cleaning and extracting syracuse 2016
-syr_16 <- pdf_text("/Users/michaeltopper/Desktop/Fraternities and Sexual Assault/Data/campus_daily_crime_log/Syracuse/2016 DCL.pdf")
+syr_16 <- pdf_text("Data/campus_daily_crime_log/Syracuse/2016 DCL.pdf")
 
-syr_16 %>% str_split('\n') %>% unlist() %>% str_trim() ->syr_16
+syr_16 %>% str_split('\n') %>% unlist() %>% str_trim() %>% str_to_lower() ->syr_16
 
 syr_16 %>% 
   str_detect("^.{1,}\\d{1,2}/\\d{1,2}/\\d{1,2}.{1,}\\d{1,2}:\\d{1,2}:\\d{1,2}") %>% which -> incidents
@@ -60,7 +60,7 @@ syra_incident <- syr_16[incidents] %>%
   as_tibble() %>% 
   separate(value, c("location", "other"), "\\s{2,}", extra = "merge") %>% 
   extract(other, c("incident", "date_occurred", "time_occurred", "date_reported", "time_reported"),
-          "(.{1,})\\s{1,}(\\d{1,2}/\\d{1,2}/\\d{4}).{1,}(\\d{1,2}:\\d{1,2}:\\d{1,2}).{1,}(\\d{1,2}/\\d{1,2}/\\d{4}).{0,}(\\d{1,2}:\\d{1,2}:\\d{1,2})")  %>% 
+          "(.{1,})\\s{1,}(\\d{2}/\\d{1,2}/\\d{4}).{1,}(\\d{1,2}:\\d{1,2}:\\d{1,2}).{1,}(\\d{2}/\\d{1,2}/\\d{4}).{0,}(\\d{1,2}:\\d{1,2}:\\d{1,2})")  %>% 
   mutate(across(starts_with("date"), ~ifelse(str_detect(., "^0"), paste("1", . ,sep = ""), .))) 
 syra_case_number <- syr_16[case_number] %>% 
   as_tibble() %>% 
@@ -68,13 +68,14 @@ syra_case_number <- syr_16[case_number] %>%
 
 syra_16 <- bind_cols(syra_incident, syra_case_number)
 
+
 syracuse_16 <- syra_16 %>% 
   mutate(across(starts_with("time"), ~format(strptime(., "%H:%M:%S"), format = '%H:%M'))) %>% 
   mutate(university = "Syracuse University") %>% 
   mutate(across(starts_with("time"), ~as.character(.)))
 
 ## cleaning 2017 - split into two files because formatting changes
-syra_17 <- pdf_text("/Users/michaeltopper/Desktop/Fraternities and Sexual Assault/Data/campus_daily_crime_log/Syracuse/2017 DCL.pdf")
+syra_17 <- pdf_text("Data/campus_daily_crime_log/Syracuse/2017 DCL.pdf")
 
 syra_17 %>% str_split('\n') %>% unlist() %>% str_trim()-> syra_17
 
@@ -87,7 +88,7 @@ syracuse_17_1 <- syra_17 %>%
   select(-open, -date_disposition) %>% 
   mutate(time_occurred = NA, university = "Syracuse University", location = NA)
 
-path_2017_2 <- "/Users/michaeltopper/Desktop/Fraternities and Sexual Assault/Data/campus_daily_crime_log/Syracuse/2017 DCL_2.xlsx"
+path_2017_2 <- "Data/campus_daily_crime_log/Syracuse/2017 DCL_2.xlsx"
 
 syracuse_17_2 <- read_xlsx(path_2017_2, skip = 4) %>% 
   janitor::clean_names()
@@ -104,7 +105,7 @@ syracuse_17_2 <- syracuse_17_2 %>%
 
 
 ## cleaning 2018
-syra_18 <- pdf_text("/Users/michaeltopper/Desktop/Fraternities and Sexual Assault/Data/campus_daily_crime_log/Syracuse/2018 DCL.pdf")
+syra_18 <- pdf_text("Data/campus_daily_crime_log/Syracuse/2018 DCL.pdf")
 
 syra_18 %>% str_split('\n') %>% unlist() %>% str_trim()-> syra_18
 
@@ -120,7 +121,7 @@ syracuse_18 <- syra_18 %>%
 
 ### 2019
 
-path_2019 <- "/Users/michaeltopper/Desktop/Fraternities and Sexual Assault/Data/campus_daily_crime_log/Syracuse/DCL 2019.xlsx"
+path_2019 <- "Data/campus_daily_crime_log/Syracuse/DCL 2019.xlsx"
 
 syracuse_19 <- read_xlsx(path_2019) %>% 
   janitor::clean_names()
@@ -149,5 +150,5 @@ syracuse <- syracuse %>%
   group_by(across(-incident)) %>% 
   summarize(incident = paste(incident, collapse = " "))
 
-write_csv(syracuse, file = "/Users/michaeltopper/Desktop/Fraternities and Sexual Assault/Data/campus_daily_crime_log/Cleaned_schools/syracuse.csv")
+write_csv(syracuse, file = "Data/campus_daily_crime_log/Cleaned_schools/syracuse.csv")
 
