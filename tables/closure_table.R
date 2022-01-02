f947d29a7e7bd2bb99e1445f54a3848a720b3b34
Michael Topper <miketopper123@gmail.com>
2022-01-02 13:59:42 -0800

    paper finally ready

--- tables/closure_table.R
+++ tables/closure_table.R
@@ -18,7 +18,7 @@ closure_table <- daily_crime %>%
 closure_table <- kbl(closure_table, booktabs = T,
     col.names = c("University", "Moratorium 1 Start", "Moratorium 1 End", "Moratorium 2 Start", "Moratorium 2 End", "Moratorium 3 Start", "Moratorium 3 End"),
     caption = "\\label{closure_table}Moratorium dates of each university in the sample.") %>% 
-  kable_styling(latex_options = "scale_down") %>% 
+  kable_styling(latex_options = c("scale_down", "HOLD_position")) %>% 
   footnote(list("Universities can have multiple moratoriums in the sample period. Each moratorium date was verified by either a Fraternity and Sorority Life advisor, a news article, or a Freedom of Information Request. However, the first San Diego State University moratorium end date could not be directly verified by either a fraternity or sorority advisor, news article, or public record request. However, based on the following news article link, I am confident that the moratorium ended before the start of the 2015 semester. Link: https://newscenter.sdsu.edu/sdsu_newscenter/news_story.aspx?sid=75357"),
                threeparttable = T)
 
