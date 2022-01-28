09093220a3567f74c558321de33238b272d79a72
Michael Topper <miketopper123@gmail.com>
2022-01-27 17:38:41 -0800

    presentation

---
+++ advancement_presentation/tables/summary_stats_university.R
@@ -0,0 +1,40 @@
+## Purpose of script:
+##
+## Author: Michael Topper
+##
+## Date Last Edited: 2022-01-27
+##
+
+library(tidyverse)
+library(modelsummary)
+library(kableExtra)
+
+if(!exists("daily_crime")) {
+  daily_crime <- read_csv("created_data/xmaster_data/daily_panel.csv") %>% 
+    filter(university %in% ifc::moratorium_schools())
+}
+
+
+
+summary_stats_university <- daily_crime %>%
+  mutate(private = ifelse(control_of_institution != "Public", 1, 0)) %>% 
+  datasummary((`Total Enrollment` = total_enrollment) + 
+                (`Total Undergrad Enrollment`=undergraduate_enrollment) + 
+                (`Fraction Asian` = frac_undergrad_asian) +
+                (`Fraction Black`= frac_undergrad_black) + 
+                (`Fraction Hispanic` = frac_undergrad_hispanic_latino) +
+                (`Fraction White` = frac_undergrad_white) +
+                (`Graduation Rate` = graduation_rate_total_cohort) + 
+                (`Fraction Admitted` = frac_admitted_total) +
+                (`Fraction Private`=private) ~ (Mean + SD + Median + Min + Max ), data = .,
+              title = "Summary Statistics of the Universities in the Sample.", output = 'html')
+
+
+moratoriums <- ifc::moratorium_lengths()
+moratoriums <- moratoriums %>% 
+  group_by(university) %>% 
+  mutate(moratorium_id = n()) %>% 
+  ungroup()
+moratorium_summary <- moratoriums %>% 
+  datasummary((`Number of Moratoriums per-University` = moratorium_id) +(`Length of Moratoriums` = length_moratorium) ~ (Mean + SD + Median + Min + Max ), data = .,
+              title = "Summary Statistics of Moratoriums") 
