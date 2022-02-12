f52303d07c6d3c8f107bdd94f33ee19cfb38a565
Michael Topper <miketopper123@gmail.com>
2022-02-12 00:38:34 -0800

    game days data

--- code/cleaning_crime_log_files/create_panel_semesters_2.R
+++ code/cleaning_crime_log_files/create_panel_semesters_2.R
@@ -594,6 +594,19 @@ daily_panel_allschools <- daily_panel_allschools %>%
          two_weeks_after = laag(final_day_moratorium, c(8:14))) %>% 
   ungroup()
 
+
+# adding in football games ------------------------------------------------
+
+football_schools <- read_csv("created_data/xmaster_data/football_schools.csv")
+
+daily_panel <- daily_panel %>% 
+  left_join(football_schools, by = c("university" = "school", "date" = "game_date")) %>% 
+  mutate(game_occurred = ifelse(is.na(game_occurred), 0, game_occurred))
+
+daily_panel_allschools <- daily_panel_allschools %>% 
+  left_join(football_schools, by = c("university" = "school", "date" = "game_date")) %>% 
+  mutate(game_occurred = ifelse(is.na(game_occurred), 0, game_occurred))
+
 daily_panel_weekdays <- daily_panel %>% 
   filter(day_of_week == "Mon" | day_of_week == "Thu" | day_of_week == "Wed" | day_of_week == "Tue" )
 daily_panel_weekends <- daily_panel %>%
--- code/cleaning_football/clean_football.R
+++ code/cleaning_football/clean_football.R
@@ -18,7 +18,9 @@ football_schools <- football_schools %>%
 
 football_schools <- football_schools %>% 
   mutate(school = case_when(
+    school == "Arizona" ~ "University of Arizona" ,
     school == "Arkansas State" ~ "Arkansas State University-Main Campus",
+    school == "Arkansas" ~ "University of Arkansas" , 
     school == "Ball State" ~ "Ball State University"  , 
     school == "California" ~ "University of California-Berkeley",
     school == "Buffalo" ~"University at Buffalo" ,
@@ -29,13 +31,20 @@ football_schools <- football_schools %>%
     school == "Florida International" ~ "Florida International University", 
     school == "Florida State" ~ "Florida State University", 
     school == "Idaho" ~ "University of Idaho" , 
+    school == "Illinois" ~"University of Illinois Urbana-Champaign" , 
     school == "Indiana" ~ "Indiana University-Bloomington", 
     school == "Iowa" ~ "University of Iowa", 
     school == "Kansas" ~ "University of Kansas", 
+    school == "Kentucky" ~ "University of Kentucky" , 
     school == "LSU" ~ "Louisiana State University and Agricultural & Mechanical College",
+    school == "Louisiana" ~ "University of Louisiana at Lafayette",
     school == "Marshall" ~ "Marshall University", 
     school == "Michigan" ~"University of Michigan-Ann Arbor", 
+    school == "Michigan State" ~ "Michigan State University" , 
     school == "Missouri" ~ "University of Missouri-Columbia", 
+    school == "Ole Miss" ~ "University of Mississippi" , 
+    school == "Nebraska" ~ "University of Nebraska-Lincoln" , 
+    school == "Nevada" ~ "University of Nevada-Reno", 
     school == "New Mexico" ~ "University of New Mexico-Main Campus", 
     school == "North Carolina State" ~ "North Carolina State University at Raleigh", 
     school == "Ohio" ~ "Ohio University-Main Campus", 
@@ -43,12 +52,21 @@ football_schools <- football_schools %>%
     school == "Pitt" ~ "University of Pittsburgh-Pittsburgh Campus",
     school == "Rutgers" ~ "Rutgers University-New Brunswick",
     school == "San Diego State" ~ "San Diego State University" ,
+    school == "South Carolina" ~ "University of South Carolina-Columbia", 
+    school == "SMU" ~ "Southern Methodist University", 
     school == "Syracuse" ~ "Syracuse University", 
+    school == "Texas A&M" ~ "Texas A & M University-College Station", 
+    school == "Texas" ~ "The University of Texas at Austin" , 
+    school == "TCU" ~ "Texas Christian University", 
     school == "Texas State" ~ "Texas State University",
+    school == "Texas Tech" ~ "Texas Tech University" , 
+    school == "USC" ~ "University of Southern California", 
     school == "Virginia" ~ "University of Virginia-Main Campus",
     school == "Washington State" ~ "Washington State University" ,
-    school == "West Virginia"~ "West Virginia University"
+    school == "West Virginia"~ "West Virginia University",
+    school == "Wisconsin" ~ "University of Wisconsin-Madison" , 
   )) 
+
 
 football_schools <- football_schools %>% 
   mutate(game_date = lubridate::ymd(game_date)) %>% 
@@ -56,7 +74,8 @@ football_schools <- football_schools %>%
   mutate(away_game = ifelse(str_detect(home_away, "@"), 1, 0)) %>% 
   mutate(away_game = ifelse(is.na(away_game), 0, away_game)) %>% 
   select(-cmp, -att, -pct, -yds, -td, - game_number, - rank) %>% 
-  rename(win_loss = x7)
+  rename(win_loss = x7) %>% 
+  mutate(game_occurred = 1)
 
 
 football_schools %>% 
