40555087c62155d322e8b8f5d8e3b5d6e46e0ef2
Michael Topper <miketopper123@gmail.com>
2022-11-30 00:54:33 -0800

    update: new treatment passing final

---
+++ referee_analysis/treatment_passing_weekly.R
@@ -0,0 +1,230 @@
+library(tidyverse)
+library(fixest)
+library(modelsummary)
+library(kableExtra)
+library(patchwork)
+
+if (!exists("daily_crime")){
+  daily_crime <- read_csv("created_data/xmaster_data/daily_panel.csv")
+}
+
+
+# function for binned lags ------------------------------------------------
+
+laag <- function(x, v){
+  xx <- rep(0, length(x))
+  for(i in v){
+    xx <- xx + dplyr::lag(x, i)
+  }
+  xx[is.na(xx)] <- 0
+  xx
+}
+
+
+# creating the passing treatment variables --------------------------------
+
+daily_crime_passing <- daily_crime %>% 
+  group_by(university) %>% 
+  arrange(date) %>% 
+  mutate(mstart_1 = ifelse(closure_1 == date,1 ,0),
+         mstart_2 = ifelse(closure_2 == date, 1, 0),
+         mstart_3 = ifelse(closure_3 == date, 1, 0)) %>% 
+  mutate(mstart_1 = ifelse(university == "Florida International University" & date == "2018-01-04",
+                           1, mstart_1)) %>% 
+  mutate(across(c(mstart_1, mstart_2, mstart_3), ~ifelse(is.na(.), 0, .))) %>% 
+  mutate(weeks1 = laag(mstart_1, c(0:7)),
+         weeks2 = laag(mstart_1, c(8:14)),
+         weeks3 = laag(mstart_1, c(15:21)),
+         weeks4 = laag(mstart_1, c(22:28)),
+         weeks5 = laag(mstart_1, c(29:35)),
+         weeks6 = laag(mstart_1, c(36:42)),
+         weeks7 = laag(mstart_1, c(43:49)),
+         weeks8 = laag(mstart_1, c(50:56)),
+         weeks9 = laag(mstart_1, c(57:63))) %>%
+  mutate(weeks1_2 = laag(mstart_2, c(0:7)),
+         weeks2_2 = laag(mstart_2, c(8:14)),
+         weeks3_2 = laag(mstart_2, c(15:21)),
+         weeks4_2 = laag(mstart_2, c(22:28)),
+         weeks5_2 = laag(mstart_2, c(29:35)),
+         weeks6_2 = laag(mstart_2, c(36:42)),
+         weeks7_2 = laag(mstart_2, c(43:49)),
+         weeks8_2 = laag(mstart_2, c(50:56)),
+         weeks9_2 = laag(mstart_2, c(57:63))) %>% 
+  mutate(weeks1_3 = laag(mstart_3, c(0:7)),
+         weeks2_3 = laag(mstart_3, c(8:14)),
+         weeks3_3 = laag(mstart_3, c(15:21)),
+         weeks4_3 = laag(mstart_3, c(22:28)),
+         weeks5_3 = laag(mstart_3, c(29:35)),
+         weeks6_3 = laag(mstart_3, c(36:42)),
+         weeks7_3 = laag(mstart_3, c(43:49)),
+         weeks8_3 = laag(mstart_3, c(50:56)),
+         weeks9_3 = laag(mstart_3, c(57:63))) %>% 
+  mutate(across(starts_with("weeks"), ~ifelse(treatment == 1 & . == 1, ., 0))) %>% 
+  mutate(weeks1_final = ifelse(weeks1 == 1 | weeks1_2 == 1 | weeks1_3 == 1, 1, 0),
+         weeks2_final = ifelse(weeks2 == 1 | weeks2_2 == 1 | weeks2_3 == 1, 1, 0),
+         weeks3_final = ifelse(weeks3 == 1 | weeks3_2 == 1 | weeks3_3 == 1, 1, 0),
+         weeks4_final = ifelse(weeks4 == 1 | weeks4_2 == 1 | weeks4_3 == 1, 1, 0),
+         weeks5_final = ifelse(weeks5 == 1 | weeks5_2 == 1 | weeks5_3 == 1, 1, 0),
+         weeks6_final = ifelse(weeks6 == 1 | weeks6_2 == 1 | weeks6_3 == 1, 1, 0),
+         weeks7_final = ifelse(weeks7 == 1 | weeks7_2 == 1 | weeks7_3 == 1, 1, 0),
+         weeks8_final = ifelse(weeks8 == 1 | weeks8_2 == 1 | weeks8_3 == 1, 1, 0),
+         weeks9_final = ifelse(weeks9 == 1 | weeks9_2 == 1 | weeks9_3 == 1, 1, 0),
+         weeks9plus_final = ifelse(treatment ==1 &   (weeks1_final != 1  &
+                                                        weeks2_final != 1 &
+                                                        weeks3_final != 1 &
+                                                        weeks4_final != 1 &
+                                                        weeks5_final != 1 &
+                                                        weeks6_final != 1 &
+                                                        weeks7_final != 1 &
+                                                        weeks8_final != 1 &
+                                                        weeks9_final != 1), 1, 0))
+
+
+# fixing louisiana universitiy because of awkward timings -----------------
+
+daily_crime_passing <- daily_crime_passing %>% 
+  mutate(weeks5_final = ifelse(university =="Louisiana State University and Agricultural & Mechanical College" &
+                                 weeks1_final ==1 & 
+                                 weeks5_final == 1, 0, weeks5_final),
+         weeks6_final = ifelse(university =="Louisiana State University and Agricultural & Mechanical College" &
+                                 weeks1_final ==1 & 
+                                 weeks6_final == 1, 0, weeks6_final),
+         weeks7_final = ifelse(university =="Louisiana State University and Agricultural & Mechanical College" &
+                                 weeks2_final ==1 & 
+                                 weeks7_final == 1, 0, weeks7_final),
+         weeks8_final = ifelse(university =="Louisiana State University and Agricultural & Mechanical College" &
+                                 weeks3_final ==1 & 
+                                 weeks8_final == 1, 0, weeks8_final),
+         weeks9_final = ifelse(university =="Louisiana State University and Agricultural & Mechanical College" &
+                                 weeks4_final ==1 & 
+                                 weeks9_final == 1, 0, weeks9_final))
+
+
+# getting estimation variables --------------------------------------------
+
+fixed_effects_preferred <- c("day_of_week", "university_by_academic_year", "holiday", "spring_semester", "game_occurred")
+
+explanatory_vars <- c("weeks1_final", "weeks2_final", 
+                        "weeks3_final", "weeks4_final",
+                        "weeks5_final", "weeks6_final",
+                        "weeks7_final", "weeks8_final",
+                        "weeks9_final",
+                        "weeks9plus_final")
+
+
+
+# creating functions for list of universities and number ------------------
+
+## gets list of universities for each count
+passing <- function(data, week_variable){
+  unis <- data %>% 
+    group_by(university) %>% 
+    count({{week_variable}}) %>% 
+    filter({{week_variable}} == 1) %>% 
+    pull(university)
+  return(unis)
+}
+
+## gets counts for number of universities within each week
+passing_count <- function(data, week_variable){
+  week_variable = as.symbol(week_variable)
+  unis <- data %>% 
+    group_by(university) %>% 
+    count({{week_variable}}) %>% 
+    filter({{week_variable}} == 1) %>% 
+    pull(university)
+  return(unis)
+}
+
+## universities with over 2 months of moratoriums
+two_month <- daily_crime_passing %>% 
+  passing(weeks9plus_final)
+
+
+# getting counts of universities within each week indicator ---------------
+
+week_counts <- map(list("weeks1_final", "weeks2_final", "weeks3_final",
+     "weeks4_final", "weeks5_final", "weeks6_final",
+     "weeks7_final", "weeks8_final", "weeks9_final",
+     "weeks9plus_final"), ~daily_crime_passing %>% 
+  passing_count(.x) %>% length()) %>% 
+  unlist()
+
+week_counts_over_two_months <- map(list("weeks1_final", "weeks2_final", "weeks3_final",
+                                        "weeks4_final", "weeks5_final", "weeks6_final",
+                                        "weeks7_final", "weeks8_final", "weeks9_final",
+                                        "weeks9plus_final"), ~daily_crime_passing %>% 
+                                     filter(university %in% two_month) %>% 
+                                     passing_count(.x) %>% length()) %>% 
+  unlist()
+
+
+
+# alcohol estimation and graph --------------------------------------------
+
+alc_passing_1 <- ifc::reghdfe(daily_crime_passing , 
+             "alcohol_offense_per25", explanatory_vars,
+             fixed_effects_preferred, "university") %>% 
+  broom::tidy(conf.int = T) %>% 
+  mutate(week = row_number(),
+         university_count = week_counts) %>% 
+  mutate(week_labels = glue::glue("Moratorium\nWeek {week}\n({university_count})")) %>% 
+  mutate(week_labels = ifelse(row_number() ==10, glue::glue("Moratorium\nWeeks 10+\n({university_count})"),week_labels)) %>%
+  mutate(model = "Universities With Moratorius Over 2-months")
+
+alc_passing_graph <- ifc::reghdfe(daily_crime_passing %>% 
+               filter(university %in% two_month), 
+             "alcohol_offense_per25", explanatory_vars,
+             fixed_effects_preferred, "university") %>% 
+  broom::tidy(conf.int = T) %>% 
+  mutate(week = row_number(),
+         university_count = week_counts_over_two_months) %>% 
+  mutate(week_labels = glue::glue("Moratorium\nWeek {week}\n({university_count})")) %>%
+  mutate(week_labels = ifelse(row_number() ==10, glue::glue("Moratorium\nWeeks 10+\n({university_count})"),week_labels)) %>%
+  mutate(model = "All Universities") %>% 
+  bind_rows(alc_passing_1) %>% 
+  ggplot(aes(week_labels, estimate)) +
+  geom_point() +
+  geom_line(aes(group = 1), linetype = "dashed", alpha = 0.5) +
+  geom_errorbar(aes(ymin = conf.low, ymax = conf.high), width = 0.1) +
+  geom_hline(yintercept = 0, color = "dark red") +
+  facet_wrap(~rev(model), ncol = 1, scales = "free_x") +
+  labs(x = "", y = "Point Estimate and 95% Confidence Interval") +
+  theme_minimal()
+
+
+
+
+# sexual assault estimation and graph -------------------------------------
+
+sex_passing_1 <- ifc::reghdfe(daily_crime_passing , 
+                              "sexual_assault_per25", explanatory_vars,
+                              fixed_effects_preferred, "university") %>% 
+  broom::tidy(conf.int = T) %>% 
+  mutate(week = row_number(),
+         university_count = week_counts) %>% 
+  mutate(week_labels = glue::glue("Moratorium\nWeek {week}\n({university_count})")) %>% 
+  mutate(week_labels = ifelse(row_number() ==10, glue::glue("Moratorium\nWeeks 10+\n({university_count})"),week_labels)) %>%
+  mutate(model = "Universities With Moratorius Over 2-months")
+
+
+sex_passing_graph <- ifc::reghdfe(daily_crime_passing %>% 
+                                    filter(university %in% two_month), 
+                                  "sexual_assault_per25", explanatory_vars,
+                                  fixed_effects_preferred, "university") %>% 
+  broom::tidy(conf.int = T) %>% 
+  mutate(week = row_number(),
+         university_count = week_counts_over_two_months) %>% 
+  mutate(week_labels = glue::glue("Moratorium\nWeek {week}\n({university_count})")) %>%
+  mutate(week_labels = ifelse(row_number() ==10, glue::glue("Moratorium\nWeeks 10+\n({university_count})"),week_labels)) %>%
+  mutate(model = "All Universities") %>% 
+  bind_rows(sex_passing_1) %>% 
+  ggplot(aes(week_labels, estimate)) +
+  geom_point() +
+  geom_line(aes(group = 1), linetype = "dashed", alpha = 0.5) +
+  geom_errorbar(aes(ymin = conf.low, ymax = conf.high), width = 0.1) +
+  geom_hline(yintercept = 0, color = "dark red") +
+  facet_wrap(~rev(model), ncol = 1, scales = "free_x") +
+  labs(x = "", y = "Point Estimate and 95% Confidence Interval") +
+  theme_minimal()
+
--- tables/regressions_party_schools.R
+++ tables/regressions_party_schools.R
@@ -36,6 +36,20 @@ alc_party<- map(party_school_data, ~ifc::reghdfe(., c("alcohol_offense_per25"),e
 sex_party <- map(party_school_data, ~ifc::reghdfe(., c("sexual_assault_per25"),explanatory_vars, fixed_effects_preferred, "university")
 )
 
+summarystats_no_treatment_party <- daily_crime %>% 
+  filter(treatment == 0) %>% 
+  group_by(party_school) %>% 
+  summarize(across(.cols = c("alcohol_offense_per25","sexual_assault_per25"), 
+                   ~mean(., na.rm = T), 
+                   .names = "non_moratorium_avg_{.col}"))
+
+summarystats_no_treatment <- daily_crime %>% 
+  filter(treatment == 0) %>% 
+  summarize(across(.cols = c("alcohol_offense_per25","sexual_assault_per25"), 
+                   ~mean(., na.rm = T), 
+                   .names = "non_moratorium_avg_{.col}"))
+
+
 party_school_table <- ifc::main_table(alc_party, last_panel = sex_party) %>% 
   slice(1:6) %>% 
   add_row(term = "Mean of Dependent Variable", 
@@ -43,28 +57,32 @@ party_school_table <- ifc::main_table(alc_party, last_panel = sex_party) %>%
           `Model 2` = sprintf("%.3f",mean(party_school_data[[2]]$alcohol_offense_per25, na.rm = T)),
           `Model 3` = sprintf("%.3f",mean(party_school_data[[3]]$alcohol_offense_per25, na.rm = T)),
           .before = 4) %>% 
+  add_row(term = "Non-Moratorium Mean", 
+          `Model 1` = sprintf("%.3f",summarystats_no_treatment[[1]]),
+          `Model 2` = sprintf("%.3f",summarystats_no_treatment_party[[2,2]]),
+          `Model 3` = sprintf("%.3f",summarystats_no_treatment_party[[1,2]]),
+          .before = 5) %>% 
   add_row(term = "Mean of Dependent Variable", 
           `Model 1` = sprintf("%.3f",mean(daily_crime$sexual_assault_per25, na.rm = T)),
           `Model 2` = sprintf("%.3f",mean(party_school_data[[2]]$sexual_assault_per25, na.rm = T)),
           `Model 3` = sprintf("%.3f",mean(party_school_data[[3]]$sexual_assault_per25, na.rm = T)),
-          .before = 8) %>% 
+          .before = 9) %>% 
+  add_row(term = "Non-Moratorium Mean", 
+          `Model 1` = sprintf("%.3f",summarystats_no_treatment[[2]]),
+          `Model 2` = sprintf("%.3f",summarystats_no_treatment_party[[2,3]]),
+          `Model 3` = sprintf("%.3f",summarystats_no_treatment_party[[1,3]]),
+          .before = 10) %>% 
   kbl(booktabs = T, col.names = c(" ", "(1)", "(2)", "(3)"), align = "lccc",
       caption = "\\label{party_school_table}Effect of Moratoriums on Alcohol Offenses and Sexual Assault by Party School (OLS)") %>% 
-  pack_rows("Panel A: Alcohol Offenses", 1, 4, bold = F, italic = T) %>% 
-  pack_rows("Panel B: Sexual Assaults", 5, 8, bold = F, italic = T, latex_gap_space = "0.5cm") %>% 
+  pack_rows("Panel A: Alcohol Offenses", 1, 5, bold = F, italic = T) %>% 
+  pack_rows("Panel B: Sexual Assaults", 6, 10, bold = F, italic = T, latex_gap_space = "0.5cm") %>% 
   kable_styling(latex_options = "HOLD_position", font_size = 11) %>% 
   add_header_above(c(" ", "All Schools" = 1, "Party Schools" = 1, "Non-Party Schools" = 1), line = F) %>% 
   add_header_above(c(" " = 1, "School Type" = 3)) %>% 
-  row_spec(8, hline_after = T) %>% 
+  row_spec(10, hline_after = T) %>% 
   column_spec(1, width = "8cm") %>% 
   footnote(list("Standard errors are clustered by university and each offense is defined as per-25000 enrolled students. The column All Schools represents the preferred specification (i.e. Column 2) from the main results table which includes day of the week, football game-day, semester number, and university-by-academic-year fixed effects. A party school classification is determined from Niche.com's list of top partying schools. A university in the top 50 is considered a party school which amounts to 16 of the 37 universities.",
                 "* p < 0.1, ** p < 0.05, *** p < 0.01"),
            threeparttable = T) 
 
 
-summarystats_no_treatment_party <- daily_crime %>% 
-  filter(treatment == 0) %>% 
-  group_by(party_school) %>% 
-  summarize(across(.cols = c("alcohol_offense_per25","sexual_assault_per25"), ~mean(., na.rm = T), 
-                   .names = "non_moratorium_avg_{.col}"))
-rm(party_school_data)
